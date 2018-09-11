53ee2d1ef6aaa1a2d801734b36ae12e268fac1c3
Boonruang Seedapunt <scubatoy@gmail.com>
2018-09-11 13:10:19 +0700

    add form and create UsersRequest

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Role;
+use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -17,7 +19,9 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.index');
+        $users = User::all();
+
+        return view('admin.users.index',compact('users'));
     }
 
     /**
@@ -28,7 +32,10 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
-        return view('admin.users.create');
+
+        $roles = Role::lists('name','id')->all();
+
+        return view('admin.users.create',compact('roles'));
     }
 
     /**
@@ -40,6 +47,9 @@ class AdminUsersController extends Controller
     public function store(Request $request)
     {
         //
+
+        return $request->all();
+
     }
 
     /**
