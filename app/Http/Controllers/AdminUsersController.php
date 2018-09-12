eba5b80dfad483e205710b6298d7fdf5aa7653ee
Boonruang Seedapunt <scubatoy@gmail.com>
2018-09-12 14:17:26 +0700

    implement posts model and table

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -10,6 +10,7 @@ use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Session;
 
 class AdminUsersController extends Controller
 {
@@ -164,5 +165,21 @@ class AdminUsersController extends Controller
     public function destroy($id)
     {
         //
+//       User::findOrFail($id)->delete();
+
+        $user = User::findOrFail($id);
+
+        unlink(public_path().$user->photo->file);
+
+        $user->delete();
+
+
+        Session::flash('deleted_user','The user has been deleted');
+
+        return redirect('/admin/users');
+
+//        return "DESTROY";
+
+
     }
 }
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1 class="text-center">Create posts</h1>
+
+
+@stop
