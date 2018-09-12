578139b1f6988318cee22ddd902923ff3764bcb6
Boonruang Seedapunt <scubatoy@gmail.com>
2018-09-12 08:16:45 +0700

    users section working 100 percent - excecpt secuerity

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>User</h1>
+    <h1>Users</h1>
 
     <table class="table">
     <thead>
       <tr>
         <th>ID</th>
+        <th>Photo</th>
         <th>Name</th>
         <th>Email</th>
-        <th>Status</th>
+        <th>Role</th>
         <th>Active</th>
         <th>Created</th>
         <th>Updated</th>
@@ -21,9 +22,10 @@
         @foreach($users as $user)
       <tr>
         <td>{{$user->id}}</td>
-        <td>{{$user->name}}</td>
+          <td><img height="50" src="{{$user->photo ? $user->photo->file: 'http://placehold.it/400x400'}}" alt=""></td>
+        <td><a href="{{route('admin.users.edit',$user->id)}}">{{$user->name}}</a></td>
         <td>{{$user->email}}</td>
-        <td>{{$user->role->name}}</td>
+        <td>{{$user->role ? $user->role->name : 'User has no role'}}</td>
         <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
         <td>{{$user->created_at->diffForHumans()}}</td>
         <td>{{$user->updated_at->diffForHumans()}}</td>
